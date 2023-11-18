5bea222540829c6511e547b877acf4124883a0a4
daron976 <daronkwiafe@gmail.com>
2023-11-18 22:51:34 +0200

    update api content

--- src/app/api/v1/[endpoint]/route.js
+++ src/app/api/v1/[endpoint]/route.js
@@ -1,73 +1,71 @@
-import { notFound, redirect } from "next/navigation";
-
-export const dynamic = "force-dynamic";
+import { notFound } from "next/navigation";
+import { LIQABI } from "../../utils/api-utils";
+import Web3 from "web3";
+import BigNumber from "bignumber.js";
 
 export async function GET(request, { params }) {
   const endpoint = params.endpoint;
   const contractAddress = "0x2749C9f2f8568d389BBF61ed77784A43C3cD3E19";
+  const holderInfo = [
+    "0x407993575c91ce7643a4d4ccacc9a98c36ee1bbe",
+    "0xaf967c1a979d4600affce6bffbaeacfd165a1a2a",
+    "0x2ddfda4a037836bb5f78075d6bc356d6ae06fd9b",
+    "0x079ef53e8533fac72079930a34b380145f797471",
+  ];
 
+  // web3 instance
+  const web3 = new Web3(
+    new Web3.providers.HttpProvider("https://bsc-dataseed.binance.org/")
+  );
+
+  // contract instance
+  const tokenContract = new web3.eth.Contract(LIQABI, contractAddress);
+
+  const result = await tokenContract.methods.totalSupply().call();
+
+  // token decimal
+  const decimal = await tokenContract.methods.decimals().call();
+
+  const decimalVal = Math.pow(10, BigNumber(decimal));
+
+  // api responses
   switch (endpoint) {
     case "total_supply":
-      const res = await fetch(
-        `https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=${contractAddress}&apikey=SYTBNFA6AVVKBHFQPUASSDQZY8FXWY3TA5`
-      );
-      const data = await res.json();
-      return Response.json(data);
-    case "circulating_supply":
-      let holderTotal = [];
-      const holderInfo = [
-        {
-          contractAddress,
-          address: "0x407993575c91ce7643a4d4ccacc9a98c36ee1bbe",
-        },
-        {
-          contractAddress,
-          address: "0xaf967c1a979d4600affce6bffbaeacfd165a1a2a",
-        },
-        {
-          contractAddress,
-          address: "0x2ddfda4a037836bb5f78075d6bc356d6ae06fd9b",
-        },
-        {
-          contractAddress,
-          address: "0x079ef53e8533fac72079930a34b380145f797471",
-        },
-      ];
-      for (let i = 0; i < holderInfo.length; i++) {
-        await fetch(
-          `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${contractAddress}&address=${holderInfo[i].address}&tag=latest&apikey=SYTBNFA6AVVKBHFQPUASSDQZY8FXWY3TA5`
-        )
-          .then((res) => res.json())
-          .then((json) => holderTotal.push(BigInt(json.result)));
-      }
-      const holderMaxTotal = holderTotal.reduce((a, b) => a + b);
+      const convertedResult = new BigNumber(result).toFixed();
 
-      const tsupply = await fetch(
-        `https://api.bscscan.com/api?module=stats&action=tokensupply&contractaddress=${contractAddress}&apikey=SYTBNFA6AVVKBHFQPUASSDQZY8FXWY3TA5`
-      );
-      const result = await tsupply.json();
-
-      const circulation = BigInt(result.result) - holderMaxTotal;
+      const finalResult = (convertedResult / decimalVal).toFixed(2);
 
       return Response.json({
         status: 1,
         message: "ok",
-        result: BigInt(circulation).toString(),
+        result: parseFloat(finalResult),
       });
+
+    case "circulating_supply":
+      let holderTotal = [];
+
+      for (let i = 0; i < holderInfo.length; i++) {
+        await tokenContract.methods
+          .balanceOf(holderInfo[i])
+          .call()
+          .then((res) => holderTotal.push(res));
+      }
+
+      const holderMaxTotal = holderTotal.reduce((a, b) => a + b);
+
+      const convertedCirculation = new BigNumber(
+        result - holderMaxTotal
+      ).toFixed();
+
+      const finalCirculation = (convertedCirculation / decimalVal).toFixed(2);
+
+      return Response.json({
+        status: 1,
+        message: "ok",
+        result: parseFloat(finalCirculation),
+      });
+
     default:
-      redirect("/");
+      notFound();
   }
 }
-
-// async function getData() {
-
-//   for (let i = 0; i < holderInfo.length; i++) {
-//     await fetch(
-//       `https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${contractAddress}&address=${holderInfo[i].address}&tag=latest&apikey=SYTBNFA6AVVKBHFQPUASSDQZY8FXWY3TA5`
-//     )
-//       .then((res) => res.json())
-//       .then((json) => holderTotal.push(parseInt(json.result)));
-//   }
-//   const holderMaxTotal = holderTotal.reduce((a, b) => a + b);
-//   return { contractResult, holderMaxTotal };
-// }
